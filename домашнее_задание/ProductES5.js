26638d22ab6cfe5af233075304856df52738e105
Pavel Remizov <remizov.p1@azurgames.com>
2022-03-08 23:58:28 +0300

    Class and constructor

---
+++ домашнее_задание/ProductES5.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+function ProductES5(productName, productPrice) {
+    this.productName = productName;
+    this.productPrice = productPrice;
+}
+
+ProductES5.prototype.make25Discount = function () {
+    this.productPrice *= 0.75;
+};
+
+ProductES5.prototype.printData = function () {
+    console.log(`${this.productName} ${this.productPrice}`);
+};
---
+++ домашнее_задание/ProductES6.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+class ProductES6{
+    constructor(productName, productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    make25Discount() {
+        this.productPrice *= 0.75;
+    }
+
+    printData() {
+        console.log(`${this.productName} ${this.productPrice}`);
+    }
+}
