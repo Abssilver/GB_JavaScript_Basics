fafb191e1d80eb37d44eb0dd33531384f232f5e1
Pavel Remizov <remizov.p1@azurgames.com>
2022-03-09 01:02:02 +0300

    Class extends and fixes

---
+++ домашнее_задание/PostES5.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+function PostES5(author, text) {
+    this.author = author;
+    this.text = text;
+    this.date = new Date();
+}
+
+PostES5.prototype.edit = function (text) {
+    this.text = text;
+    this.date = new Date();
+};
+
+PostES5.prototype.printData = function () {
+    console.log(`${this.author} ${this.text} ${this.date}`);
+};
---
+++ домашнее_задание/PostES6.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+class PostES6{
+    constructor(author, text) {
+        this.author = author;
+        this.text = text;
+        this.date = new Date();
+    }
+
+    edit(text) {
+        this.text = text;
+        this.date = new Date();
+    }
+
+    printData() {
+        console.log(`${this.author} ${this.text} ${this.date}`);
+    }
+}
