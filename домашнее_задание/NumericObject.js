378114fca2bfe11907352b7713db16dc841ff625
Pavel Remizov <remizov.p1@azurgames.com>
2022-03-09 01:44:50 +0300

    numeric function

---
+++ домашнее_задание/NumericObject.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+class NumericObject {
+    constructor(units, dozens, hundreds) {
+        this.units = units;
+        this.dozens = dozens;
+        this.hundreds = hundreds;
+    }
+
+    printData() {
+        console.log(`Количество сотен: ${this.hundreds}, десятков: ${this.dozens}, единиц: ${this.units}`);
+    }
+}
